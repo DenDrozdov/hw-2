fbeaf7926ec5cb4ab9b71ae24f88b008f6ae8e12
Drozdov123123 <den_drozdov_2018_2004@mail.ru>
2026-02-24 21:36:53 +0700

    Its done?

---
+++ composeApp/src/iosMain/kotlin/com/example/ktshw/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.ktshw
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ shared/src/commonMain/kotlin/com/example/ktshw/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.ktshw
+
+const val SERVER_PORT = 8080
